eda5e3f21f61595bb87464dd721c5d3b8c8433a1
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2023-07-31 18:17:02 -0400

    organization, state, and new form
    coordinated spacing/tab size across files, added new expense folder and component to add new forms, and added useState for ExpenseItem component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,20 @@
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
     function clickHandler() {
-        console.log('Clicked!')
+        setTitle('Updated!');
+        console.log('ExpenseItem evaluated');
     }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
