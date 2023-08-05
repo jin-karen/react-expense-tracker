27ee56b598fd61f0f95f6ecb7823d5eb84f40448
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2023-08-05 02:14:48 -0400

    add expenses list component
    cleaned up expenses component by adding expenseslist component and included conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2023');
@@ -10,19 +10,7 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
         console.log(selectedYear);
     };
-    const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear() == filteredYear);
-    
-    let expensesContent = <p>No expenses found.</p>
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-                title={expense.title} 
-                amount={expense.amount} 
-                date={expense.date}
-            />
-		));
-	}
+    const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear().toString() === filteredYear);
 
     return (
         <Card className="expenses">
@@ -30,7 +18,7 @@ function Expenses(props) {
                 selected={filteredYear} 
                 onFilterYear={filterChangeHandler} 
             />
-            {expensesContent}
+            <ExpensesList filteredExpenses={filteredExpenses} /> 
         </Card>
     );
 }
