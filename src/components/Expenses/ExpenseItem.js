6524e39abd51a6f6b4464ad64c7768cfa7c4030f
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2023-08-08 01:29:00 -0400

    edit style
    redo color palette for entire project

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,20 +5,14 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
-    function clickHandler() {
-        setTitle('Updated!');
-        console.log('ExpenseItem evaluated');
-    }
     return (
         <li>
             <Card className="expense-item">
                 <ExpenseDate date={props.date} />
                 <div className="expense-item__description">
-                    <h2>{title}</h2>
+                    <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
-                <button onClick={clickHandler}>Change Title</button>
             </Card>
         </li>
     ); 
